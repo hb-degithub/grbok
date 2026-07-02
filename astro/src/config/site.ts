701a0e64b0145e545604e0080c89f8fec4378312
HB <670486183@qq.com>
2026-07-02 09:50:49 +0800

    chore: replace footer Twitter with Bilibili, tighten HTML cache TTL for ESA

--- astro/src/config/site.ts
+++ astro/src/config/site.ts
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Site configuration shared by layouts and components.
  */
 export const SITE_CONFIG = {
@@ -18,7 +18,7 @@ export const SITE_CONFIG = {
   friendLinks: [],
   socialLinks: [
     { name: 'github', url: 'https://github.com' },
-    { name: 'twitter', url: 'https://twitter.com' },
+    { name: 'bilibili', url: 'https://space.bilibili.com/691237475' },
   ],
 } as const;
 
@@ -26,3 +26,5 @@ export const HERO_CARDS = [
   { label: 'Articles', countKey: 'posts', icon: 'M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z' },
   { label: 'Tags', countKey: 'tags', icon: 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z' },
 ] as const;
+
+
